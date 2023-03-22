87fb69bd9bd2be4a652c8bfba397060f3a45ec24
Christian Finetti <christian.finetti@mail.polimi.it>
2023-03-22 20:30:16 +0100

    CardBox class update with method getCard

--- MyShelfie/src/main/java/it/polimi/ingsw/Cardbox.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Cardbox.java
@@ -1,10 +1,49 @@
 package it.polimi.ingsw;
 
+import java.util.Random;
+
 public class Cardbox {
     int cardLeft, leftWhite, leftBlue, leftLightBlue, leftYellow, leftPurple, leftGreen;
 
-    private Cardbox(){};
-    public Card getCard(){};
+    private Cardbox(){
+        cardLeft = 132;
+        leftWhite = 22;
+        leftBlue= 22;
+        leftLightBlue= 22;
+        leftYellow = 22;
+        leftPurple = 22;
+        leftGreen = 22;
+    }
+    public Card getCard(){
+        Random rn = new Random();
+        int rand = rn.nextInt(1,cardLeft + 1);
+        if (rand > 0 && rand <= leftWhite) {
+            cardLeft--;
+            leftWhite--;
+            return Card.WHITE;
+        } else if(rand > leftWhite && rand <= leftWhite + leftBlue ) {
+            cardLeft--;
+            leftBlue--;
+            return Card.BLUE;
+        } else if(rand > leftWhite + leftBlue && rand <= leftWhite + leftBlue + leftLightBlue) {
+            cardLeft--;
+            leftLightBlue--;
+            return Card.LIGHTBLUE;
+        } else if(rand > leftWhite + leftBlue + leftLightBlue && rand <= leftWhite + leftBlue + leftLightBlue + leftYellow) {
+            cardLeft--;
+            leftYellow--;
+            return Card.YELLOW;
+        } else if(rand > leftWhite + leftBlue + leftLightBlue + leftYellow && rand <= leftWhite + leftBlue + leftLightBlue + leftYellow + leftPurple) {
+            cardLeft--;
+            leftPurple--;
+            return Card.PURPLE;
+        } else {
+            cardLeft--;
+            leftGreen--;
+            return Card.GREEN;
+        }
+    
 
+    }
 
 }
