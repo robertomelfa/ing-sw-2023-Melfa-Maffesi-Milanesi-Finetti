16d7e4b1e290f81cc0ce31c48c855068aa77ac7b
robertomelfa <roberto.melfa@mail.polimi.it>
2023-06-02 15:29:26 +0200

    Fixed checknear3

--- MyShelfie/src/main/java/it/polimi/ingsw/Model/GameLogic.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/GameLogic.java
@@ -183,7 +183,7 @@ public class GameLogic implements Serializable{
      */
     public boolean checkNear(int x1, int y1, int x2, int y2, int x3, int y3){
         if(checkNear(x1, y1) && checkNear(x2, y2) && checkNear(x3, y3)){
-            if(((checkNear(x1, y1, x2, y2) && checkNear(x2, y2, x3, y3)) || ((checkNear(x1, y1, x3, y3) && checkNear(x2, y2, x3, y3)))) && ((x1 == x2 && x2 == x3) || (y1 == y2 && y2 == y3))){
+            if(((checkNear(x1, y1, x2, y2) && checkNear(x2, y2, x3, y3)) || ((checkNear(x1, y1, x3, y3) && checkNear(x2, y2, x3, y3))) || ((checkNear(x1, y1, x3, y3) && checkNear(x1, y1, x2, y2))) && ((x1 == x2 && x2 == x3) || (y1 == y2 && y2 == y3)))){
                 return true;
             }
         }
