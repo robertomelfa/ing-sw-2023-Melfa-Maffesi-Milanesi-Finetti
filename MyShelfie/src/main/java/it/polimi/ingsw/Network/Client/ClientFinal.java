77be45b875424efd0233d98d54747e96adbbdac9
Matteo <matteo.maffesi@mail.polimi.it>
2023-06-27 15:01:47 +0200

    Added ClientFinal

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Network/Client/ClientFinal.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.Network.Client;
+
+
+public class ClientFinal {
+
+    /**
+     * method use to compile a JavaFX application because it cannot compile a class that extends Application
+     * @param args
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws java.lang.Exception{
+
+
+        ClientMain.main(args);
+    }
+}
